c7d74ffe6a0052d906f459244b1fa6e380684aa0
KartofellFirst <deznevnikita8@gmail.com>
2025-10-02 20:51:45 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+changable = [0, 0]
+
+@app.route('/')
+def home():
+  return json.dumps(changable)
+
+@app.route('/change', methods=["POST"])
+def change():
+    data = request.get_json()
+    alt = int(data.get("altitude", 404))
+    azi = int(data.get("azimuth", 404))
+    changable = [alt, azi]
+    return "ok", 200
